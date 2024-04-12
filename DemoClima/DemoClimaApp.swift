f1dfc872ae45bd0e184501729d39a1bb46529ef9
Makinadefuego <fportillap2100@alumno.ipn.mx>
2024-04-11 19:52:09 -0600

    Initial Commit

---
+++ DemoClima/DemoClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoClimaApp.swift
+//  DemoClima
+//
+//  Created by Fernando Daniel Portilla Posadas on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
